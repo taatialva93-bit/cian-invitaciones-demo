0dff4034dd274ba71b3947d5333afab330bc4b9e
taatialva93-bit <taatialva93@gmail.com>
2026-07-09 08:41:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function abrirInvitacion() {
+function abrirInvitacion(){
   const portada = document.getElementById("portada");
   const invitacion = document.getElementById("invitacion");
 
   portada.style.display = "none";
   invitacion.style.display = "block";
-
   window.scrollTo(0, 0);
 }
 
 const fechaEvento = new Date("2026-08-08T21:30:00");
 
-function actualizarContador() {
+function actualizarContador(){
   const countdown = document.getElementById("countdown");
+  if(!countdown) return;
+
   const ahora = new Date();
   const diferencia = fechaEvento - ahora;
 
-  if (diferencia <= 0) {
+  if(diferencia <= 0){
     countdown.innerHTML = "¡Llegó la gran noche!";
     return;
   }
@@ -25,8 +26,7 @@ function actualizarContador() {
   const minutos = Math.floor((diferencia / (1000 * 60)) % 60);
   const segundos = Math.floor((diferencia / 1000) % 60);
 
-  countdown.innerHTML =
-    `${dias} días · ${horas} hs · ${minutos} min · ${segundos} seg`;
+  countdown.innerHTML = `${dias} días · ${horas} hs · ${minutos} min · ${segundos} seg`;
 }
 
 actualizarContador();
